669bd4ed17cc9ec968878ec66d84399a743ccea6
elsasn <elsa.snb@gmail.com>
2021-11-12 11:55:35 +0700

    Update Scenario.

--- demo/app-booking.py
+++ demo/app-booking.py
@@ -5,9 +5,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from time import sleep
+
 driver = webdriver.Firefox(executable_path="/Users/user/Downloads/geckodriver")
 
 print("Akses URL")
+driver.maximize_window()
 driver.get("https://mamikos.com/room/kost-kabupaten-simeulue-kost-campur-eksklusif-kos-agen-duo-tenant-1#/")
 print(driver.title)
 driver.implicitly_wait(5)
@@ -18,13 +21,22 @@ driver.find_element(By.XPATH, "//button[@class='mami-button booking-card__bookin
 print("Login - Input Nomor HP dan Password")
 print(driver.current_url)
 get_username = driver.find_element(By.XPATH, '//input[@type="tel"]')
-get_username.send_keys("083159526508")
+get_username.send_keys("083865420038")
 get_password = driver.find_element(By.XPATH, '//input[@type="password"]')
 get_password.send_keys("Password123")
 submit = driver.find_element(By.XPATH, '//button[@class="btn btn-primary btn-mamigreen login-button track-login-tenant"]')
 submit.click()
-WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//span[@class="cell day"]'))).click()
-#driver.find_element(By.XPATH, "//span[@class='cell day']").click()
+sleep(3)
+#WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//span[@class="cell day"]'))).click()
+driver.find_element(By.XPATH, "//input[@class='booking-input-checkin__input']").click()
+sleep(3)
+driver.find_element(By.XPATH, "//span[@class='cell day']").click()
+sleep(3)
 driver.find_element(By.XPATH, "//div[@role='radiobutton']").click()
+sleep(3)
 driver.find_element(By.XPATH, "//button[@class='mami-button booking-card__booking-action track_request_booking mami-button--primary mami-button--large mami-button--block']").click()
+sleep(3)
+driver.find_element(By.XPATH, "//span[@class'bg-c-checkbox__icon']").click()
+sleep(3)
+driver.find_element(By.XPATH, "//button[@class='bg-c-button bg-c-button--primary bg-c-button--lg bg-c-button--block']").click()
 #driver.implicitly_wait(5)
